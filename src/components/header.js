a1210326eb2e19da8c531ef8654ee31b1c3efc30
ANIL KARLIDAG <karlidag.17982@gmail.com>
2025-04-18 17:08:02 +0300

    navigate create page

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,11 @@ import { LitElement, html } from "lit";
 export class Header extends LitElement {
   render() {
     return html`
-      <header style="background-color: #000; color: #fff;">
-      <span>HEADER 1</span>
+      <header
+        style="background-color: #000; color: #fff;display:flex;justify-content:space-between;align-items:center;"
+      >
+        <span>HEADER 1</span>
+        <a href="/create">+ Add New</a>
       </header>
     `;
   }
