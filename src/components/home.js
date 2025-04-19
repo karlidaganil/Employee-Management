b86d38691b2c8ef05e161119bac959597f1a30d8
ANIL KARLIDAG <karlidag.17982@gmail.com>
2025-04-19 23:21:15 +0300

    icons added

--- src/components/home.js
+++ src/components/home.js
@@ -2,6 +2,8 @@ import { LitElement, html } from "lit";
 import "./header/header";
 import "./table";
 import { useEmployeeStore } from "../store";
+import listIcon from "../assets/list-icon.png";
+import tableIcon from "../assets/table-icon.png";
 
 export class Home extends LitElement {
   static get properties() {
@@ -23,11 +25,13 @@ export class Home extends LitElement {
     return html`
       <div>
         <header-component></header-component>
-        <div style="display: flex; justify-content: space-between; align-items: center; padding: 0px 20px; color: orange;">
+        <div
+          style="display: flex; justify-content: space-between; align-items: center; padding: 0px 20px; color: #FF6600;"
+        >
           <h1>Employee List</h1>
-          <div>
-            <span>List Icon</span>
-            <span>Table Icon</span>
+          <div style="display: flex; gap: 10px;">
+            <img src=${listIcon} style="width: 20px; height: 20px;" />
+            <img src=${tableIcon} style="width: 20px; height: 20px;" />
           </div>
         </div>
         <div style="padding: 0px 20px;">
