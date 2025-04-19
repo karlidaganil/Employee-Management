20ea3d6c3166eb52532a43270e7d89819d9b73ad
ANIL KARLIDAG <karlidag.17982@gmail.com>
2025-04-20 00:24:20 +0300

    persist added to the store zustand

--- src/store.js
+++ src/store.js
@@ -1,38 +1,48 @@
 import { createStore } from "zustand/vanilla";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-export const useEmployeeStore = createStore((set) => ({
-  employees: [
+export const useEmployeeStore = createStore(
+  persist(
+    (set) => ({
+      employees: [
+        {
+          firstName: "John",
+          lastName: "Doe",
+          dateOfEmployment: "23/04/2025",
+          dateOfBirth: "23/04/1990",
+          phone: "1234567890",
+          email: "john.doe@example.com",
+          department: "Sales",
+          position: "Sales Manager",
+        },
+        {
+          firstName: "Jane",
+          lastName: "Doe",
+          dateOfEmployment: "23/04/2025",
+          dateOfBirth: "23/04/1995",
+          phone: "1234567890",
+          email: "jane.doe@example.com",
+          department: "Sales",
+          position: "Sales Manager",
+        },
+        {
+          firstName: "Jim",
+          lastName: "Doe",
+          dateOfEmployment: "23/04/2025",
+          dateOfBirth: "23/04/1990",
+          phone: "1234567890",
+          email: "jim.doe@example.com",
+          department: "Sales",
+          position: "Sales Manager",
+        },
+      ],
+      addEmployee: (employee) =>
+        set((state) => ({ employees: [...state.employees, employee] })),
+    }),
     {
-      firstName: "John",
-      lastName: "Doe",
-      dateOfEmployment: "23/04/2025",
-      dateOfBirth: "23/04/1990",
-      phone: "1234567890",
-      email: "john.doe@example.com",
-      department: "Sales",
-      position: "Sales Manager",
-    },
-    {
-      firstName: "Jane",
-      lastName: "Doe",
-      dateOfEmployment: "23/04/2025",
-      dateOfBirth: "23/04/1995",
-      phone: "1234567890",
-      email: "jane.doe@example.com",
-      department: "Sales",
-      position: "Sales Manager",
-    },
-    {
-      firstName: "Jim",
-      lastName: "Doe",
-      dateOfEmployment: "23/04/2025",
-      dateOfBirth: "23/04/1990",
-      phone: "1234567890",
-      email: "jim.doe@example.com",
-      department: "Sales",
-      position: "Sales Manager",
-    },
-  ],
-  addEmployee: (employee) =>
-    set((state) => ({ employees: [...state.employees, employee] })),
-}));
+      name: 'employee-storage', // unique name for localStorage key
+      storage: createJSONStorage(() => localStorage), // use localStorage
+      partialize: (state) => ({ employees: state.employees }), // only persist employees array
+    }
+  )
+);
